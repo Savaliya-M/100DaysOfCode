e5aaac8f781e02821c9010bfe817beff75e1a847
Savaliya-M <masavaliya183@gmail.com>
2022-09-29 20:43:59 +0530

    update on file one

--- reversearray.java
+++ reversearray.java
@@ -10,21 +10,22 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
 public class reversearray {
     public static void main(String[] args) throws IOException {
-        
-       Scanner sc = new Scanner(System.in);
-       
-       int n = sc.nextInt();
-       int[] arr = new int[n];
-       for(int i = 0 ; i < n ; i++){
-           arr[i] = sc.nextInt();
-       }
-       
-       for(int i = 0;i<n;i++){
-           System.out.print(arr[n-i-1]+" ");
-       }
-   
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[n - i - 1] + " ");
+        }
+
+        System.out.println("==========end=============");
+
     }
 }
