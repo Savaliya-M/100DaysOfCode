3814ee6c33eeeae550736290489530b8577ad63f
Savaliya-M <masavaliya183@gmail.com>
2022-11-01 22:56:51 +0530

    Day 12 commit

---
+++ countWordsFromCamelcaseString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class countWordsFromCamelcaseString {
+    public static int camelcase(String s) {
+        char[] ch = s.toCharArray();
+        int count = 0;
+        for(int i = 0; i < s.length();i++){
+            if(Character.isUpperCase(ch[i])){
+                count++;
+            }
+        }
+        return count+1;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the camel case string : ");
+        String s = sc.nextLine();
+        int result = camelcase(s);
+        System.out.println("word in a string = "+ result);
+    }
+}
