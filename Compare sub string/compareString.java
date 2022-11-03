4956d7df69b4a4f5dc3e566c45cdd371cc6bc419
Savaliya-M <masavaliya183@gmail.com>
2022-11-03 16:27:48 +0530

    Day 14 commit

--- Compare sub string/compareString.java
+++ Compare sub string/compareString.java
@@ -29,7 +29,7 @@ class Result {
                         return result;
                     }else{
                         result = "NO";
-                    }
+                    } 
                 }
             }
         }
