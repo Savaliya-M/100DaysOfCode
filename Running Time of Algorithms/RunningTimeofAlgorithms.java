02d9963d8dc74e7976cbbf9c0b37b7cc7987429d
Savaliya-M <masavaliya183@gmail.com>
2022-11-30 15:03:56 +0530

    Day 41 commit

---
+++ Running Time of Algorithms/RunningTimeofAlgorithms.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+public class RunningTimeofAlgorithms {
+    public static int runningTime(List<Integer> arr) {
+        int count = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            int key = arr.get(i);
+            int j = i - 1;
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
+                j--;
+                count++;
+            }
+            arr.set(j + 1, key);
+        }
+        return count;
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = RunningTimeofAlgorithms.runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
