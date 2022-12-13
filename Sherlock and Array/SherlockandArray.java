b17b0eb6e0e19f8f0c3cd8ccfdba5099641f6fc5
Savaliya-M <masavaliya183@gmail.com>
2022-12-13 14:14:00 +0530

    Day 54 commit

---
+++ Sherlock and Array/SherlockandArray.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+public class SherlockandArray {
+
+    public static String balancedSums(List<Integer> arr) {
+        int sum = 0, x = 0;
+        for (int a : arr) {
+            sum += a;
+        }
+
+        for (int y : arr) {
+            if (2 * x == sum - y) {
+                return "YES";
+            }
+            x += y;
+        }
+        return "NO";
+    }
+
+}
+
+class SherlockandArray_exe {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                String result = SherlockandArray.balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
