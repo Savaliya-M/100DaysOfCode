95b1375711136276bb07710b5fd33867beb8dd1e
Savaliya-M <masavaliya183@gmail.com>
2022-12-31 20:33:27 +0530

    Day 72 commit

---
+++ Tower Breakers/TowerBreakers.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.stream.*;
+
+public class TowerBreakers {
+    public static int towerBreakers(int n, int m) {
+        if (n % 2 == 0 || m == 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+}
+
+class towerBreakers_exe {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = TowerBreakers.towerBreakers(n, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
